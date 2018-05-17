e6cb52bc9597bd1502121c029b7982db132bd374
Tomas Nozicka <tnozicka@gmail.com>
2018-05-17 21:43:10 +0200

    Add apimachinery tools to deal with broken watches:
     - watch.RetryWatcher
     - retry.RetryWatcher
     - informer.NewInformerWatcher
     - watch.UntilWithRetry
     - watch.UntilWithInformer
     - moves watch.Until to watch.UntilWithoutRetry with a big warning not to use it
    
     Remove cache.ListWatchUntil because there is no use for it anymore

---
+++ staging/src/k8s.io/client-go/tools/watch/retrywatcher.go
@@ -0,0 +1,179 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package watch
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+)
+
+// WatcherFunc is a function that is responsible for creating a watcher starting at sinceResourceVersion.
+type WatcherFunc func(sinceResourceVersion string) (watch.Interface, error)
+
+// RetryWatcherError is the type of an event that will be returned if RetryWatcher fails.
+type RetryWatcherError struct {
+	s string
+}
+
+// Error implements error interface.
+func (e RetryWatcherError) Error() string {
+	return e.s
+}
+
+// GetObjectKind implements runtime.Object interface.
+func (e RetryWatcherError) GetObjectKind() schema.ObjectKind { return schema.EmptyObjectKind }
+
+// DeepCopyObject implements runtime.Object interface.
+func (e RetryWatcherError) DeepCopyObject() runtime.Object { return e }
+
+var _ error = RetryWatcherError{}
+var _ runtime.Object = RetryWatcherError{}
+
+// resourceVersionGetter is an interface used to get resource version from events.
+// We can't reuse an interface from meta otherwise it would be a cyclic dependency and we need just this one method
+type resourceVersionGetter interface {
+	GetResourceVersion() string
+}
+
+// RetryWatcher will make sure that in case the underlying watcher is closed (e.g. due to API timeout or etcd timeout)
+// it will get restarted from the last point without the consumer even knowing about it.
+// RetryWatcher does that by inspecting events and keeping track of resourceVersion.
+// Especially useful when using watch.Until where premature termination is causing troubles and flakes.
+// Please note that this is not resilient to ETCD cache not having the resource version anymore - you would need to
+// use Informers for that.
+type RetryWatcher struct {
+	lastResourceVersion string
+	watcherFunc         WatcherFunc
+	resultChan          chan watch.Event
+	stopChan            chan struct{}
+}
+
+// NewRetryWatcher creates a new RetryWatcher.
+// It will make sure that watcher gets restarted in case of recoverable errors.
+// The initialResourceVersion will be given to watchFunc when first called.
+func NewRetryWatcher(initialResourceVersion string, watcherFunc WatcherFunc) *RetryWatcher {
+	rw := &RetryWatcher{
+		lastResourceVersion: initialResourceVersion,
+		watcherFunc:         watcherFunc,
+		stopChan:            make(chan struct{}),
+		resultChan:          make(chan watch.Event, 0),
+	}
+	go rw.receive()
+	return rw
+}
+
+// doReceive returns true when it is done, false otherwise
+func (rw *RetryWatcher) doReceive() bool {
+	watcher, err := rw.watcherFunc(rw.lastResourceVersion)
+	if err != nil {
+		rw.resultChan <- watch.Event{
+			Type:   watch.Error,
+			Object: RetryWatcherError{fmt.Sprintf("RetryWatcher: watcherFunc failed: %v", err)},
+		}
+		// Stop the watcher
+		return true
+	}
+	ch := watcher.ResultChan()
+	defer watcher.Stop()
+
+	for {
+		select {
+		case <-rw.stopChan:
+			glog.V(5).Info("Stopping RetryWatcher.")
+			return true
+		case event, ok := <-ch:
+			if !ok {
+				glog.Warningf("RetryWatcher - getting event failed! Re-creating the watcher. Last RV: %s", rw.lastResourceVersion)
+				return false
+			}
+
+			// We need to inspect the event and get ResourceVersion out of it
+			switch event.Type {
+			case watch.Added, watch.Modified, watch.Deleted:
+				metaObject, ok := event.Object.(resourceVersionGetter)
+				if !ok {
+					rw.resultChan <- watch.Event{
+						Type:   watch.Error,
+						Object: RetryWatcherError{"__internal__: RetryWatcher: doesn't support resourceVersion"},
+					}
+					// We have to abort here because this might cause lastResourceVersion inconsistency by skipping a potential RV with valid data!
+					return true
+				}
+
+				resourceVersion := metaObject.GetResourceVersion()
+				if resourceVersion == "" {
+					rw.resultChan <- watch.Event{
+						Type:   watch.Error,
+						Object: RetryWatcherError{fmt.Sprintf("__internal__: RetryWatcher: object %#v doesn't support resourceVersion", event.Object)},
+					}
+					// We have to abort here because this might cause lastResourceVersion inconsistency by skipping a potential RV with valid data!
+					return true
+				}
+
+				// All is fine; send the event and update lastResourceVersion
+				rw.resultChan <- event
+				rw.lastResourceVersion = resourceVersion
+
+				continue
+
+			case watch.Error:
+				rw.resultChan <- event
+				return true
+
+			default:
+				glog.Errorf("RetryWatcher failed to recognize Event type %q", event.Type)
+				rw.resultChan <- watch.Event{
+					Type:   watch.Error,
+					Object: RetryWatcherError{fmt.Sprintf("__internal__: RetryWatcher failed to recognize Event type %q", event.Type)},
+				}
+				// We have to abort here because this might cause lastResourceVersion inconsistency by skipping a potential RV with valid data!
+				return true
+			}
+		}
+	}
+}
+
+// receive reads the result from a watcher, restarting it if necessary.
+func (rw *RetryWatcher) receive() {
+	for {
+		select {
+		case <-rw.stopChan:
+			glog.V(5).Info("Stopping RetryWatcher.")
+			return
+		default:
+			done := rw.doReceive()
+			if done {
+				return
+			}
+		}
+	}
+}
+
+// ResultChan implements Interface.
+func (rw *RetryWatcher) ResultChan() <-chan watch.Event {
+	return rw.resultChan
+}
+
+// Stop implements Interface.
+func (rw *RetryWatcher) Stop() {
+	close(rw.stopChan)
+}
---
+++ staging/src/k8s.io/client-go/tools/watch/retrywatcher_test.go
@@ -0,0 +1,409 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package watch
+
+import (
+	"flag"
+	"fmt"
+	"reflect"
+	"strconv"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+)
+
+type testObject struct {
+	resourceVersion string
+}
+
+func (o testObject) GetObjectKind() schema.ObjectKind { return schema.EmptyObjectKind }
+func (o testObject) DeepCopyObject() runtime.Object   { return o }
+func (o testObject) GetResourceVersion() string       { return o.resourceVersion }
+
+func withCounter(f WatcherFunc) (*uint32, func(sinceResourceVersion string) (watch.Interface, error)) {
+	var counter uint32
+	wrapped := func(sinceResourceVersion string) (watch.Interface, error) {
+		atomic.AddUint32(&counter, 1)
+		return f(sinceResourceVersion)
+	}
+
+	return &counter, wrapped
+}
+
+func makeTestEvent(rv int) watch.Event {
+	return watch.Event{
+		Type: watch.Added,
+		Object: testObject{
+			resourceVersion: fmt.Sprintf("%d", rv),
+		},
+	}
+}
+
+func AtoiOrDie(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(fmt.Errorf("failed to convert string %q to integer: %v", s, err))
+	}
+	return i
+}
+
+func arrayToChannel(array []watch.Event) chan watch.Event {
+	ch := make(chan watch.Event, len(array))
+
+	for _, event := range array {
+		ch <- event
+	}
+
+	return ch
+}
+
+func fromRV(resourceVersion string, array []watch.Event) []watch.Event {
+	if resourceVersion == "" {
+		return array
+	}
+
+	var result []watch.Event
+	rv := AtoiOrDie(resourceVersion)
+
+	for _, event := range array {
+		rvGetter, ok := event.Object.(resourceVersionGetter)
+		if ok {
+			if AtoiOrDie(rvGetter.GetResourceVersion()) <= rv {
+				continue
+			}
+		}
+
+		result = append(result, event)
+	}
+
+	return result
+}
+
+func closeAfterN(n int, source chan watch.Event) chan watch.Event {
+	result := make(chan watch.Event, 0)
+	go func() {
+		defer close(result)
+		defer close(source)
+		for i := 0; i < n; i++ {
+			result <- <-source
+		}
+	}()
+	return result
+}
+
+func TestRetryWatcher(t *testing.T) {
+	// Enable glog which is used in dependencies
+	flag.Set("logtostderr", "true")
+	flag.Set("v", "9")
+
+	tt := []struct {
+		name        string
+		initialRV   string
+		watcherFunc WatcherFunc
+		watchCount  uint32
+		result      []watch.Event
+		err         error
+	}{
+		{
+			name: "fails if watcher returns error",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return nil, fmt.Errorf("test error")
+			},
+			watchCount: 1,
+			result:     []watch.Event{},
+			err:        RetryWatcherError{"RetryWatcher: watcherFunc failed: test error"},
+		},
+		{
+			name:      "works with empty initialRV",
+			initialRV: "",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(1),
+				}))), nil
+			},
+			watchCount: 1,
+			result: []watch.Event{
+				makeTestEvent(1),
+			},
+			err: nil,
+		},
+		{
+			name:      "works with initialRV set, skipping the preceding items but reading those directly following",
+			initialRV: "1",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(1),
+					makeTestEvent(2),
+				}))), nil
+			},
+			watchCount: 1,
+			result: []watch.Event{
+				makeTestEvent(2),
+			},
+			err: nil,
+		},
+		{
+			name:      "works with initialRV set, skipping the preceding items with none following",
+			initialRV: "3",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(2),
+				}))), nil
+			},
+			watchCount: 1,
+			result:     nil,
+			err:        nil,
+		},
+		{
+			name:      "fails on RetryWatcherError",
+			initialRV: "3",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(4),
+					{Type: watch.Error, Object: RetryWatcherError{"error"}},
+				}))), nil
+			},
+			watchCount: 1,
+			result: []watch.Event{
+				makeTestEvent(4),
+			},
+			err: RetryWatcherError{"error"},
+		},
+		{
+			name:      "fails on RetryWatcherError, without reading following events",
+			initialRV: "5",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(5),
+					makeTestEvent(6),
+					{Type: watch.Error, Object: RetryWatcherError{"error"}},
+					makeTestEvent(7),
+					makeTestEvent(8),
+				}))), nil
+			},
+			watchCount: 1,
+			result: []watch.Event{
+				makeTestEvent(6),
+			},
+			err: RetryWatcherError{"error"},
+		},
+		{
+			name:      "survives 1 closed watch and reads 1 item",
+			initialRV: "5",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(1, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(6),
+				})))), nil
+			},
+			watchCount: 2,
+			result: []watch.Event{
+				makeTestEvent(6),
+			},
+			err: nil,
+		},
+		{
+			name:      "survives 2 closed watches and reads 2 items",
+			initialRV: "4",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(1, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(5),
+					makeTestEvent(6),
+				})))), nil
+			},
+			watchCount: 3,
+			result: []watch.Event{
+				makeTestEvent(5),
+				makeTestEvent(6),
+			},
+			err: nil,
+		},
+		{
+			name:      "survives 2 closed watches and reads 2 items for nonconsecutive RVs",
+			initialRV: "4",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(1, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(5),
+					makeTestEvent(7),
+				})))), nil
+			},
+			watchCount: 3,
+			result: []watch.Event{
+				makeTestEvent(5),
+				makeTestEvent(7),
+			},
+			err: nil,
+		},
+		{
+			name:      "survives 2 closed watches and reads 2 items for nonconsecutive RVs starting at much lower RV",
+			initialRV: "2",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(1, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(5),
+					makeTestEvent(7),
+				})))), nil
+			},
+			watchCount: 3,
+			result: []watch.Event{
+				makeTestEvent(5),
+				makeTestEvent(7),
+			},
+			err: nil,
+		},
+		{
+			name:      "survives 4 closed watches and reads 4 items for nonconsecutive, spread RVs",
+			initialRV: "2",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(1, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(5),
+					makeTestEvent(6),
+					makeTestEvent(7),
+					makeTestEvent(11),
+				})))), nil
+			},
+			watchCount: 5,
+			result: []watch.Event{
+				makeTestEvent(5),
+				makeTestEvent(6),
+				makeTestEvent(7),
+				makeTestEvent(11),
+			},
+			err: nil,
+		},
+		{
+			name:      "survives 4 closed watches and reads 4 items for nonconsecutive, spread RVs and skips those with lower or equal RV",
+			initialRV: "2",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(1, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(1),
+					makeTestEvent(2),
+					makeTestEvent(5),
+					makeTestEvent(6),
+					makeTestEvent(7),
+					makeTestEvent(11),
+				})))), nil
+			},
+			watchCount: 5,
+			result: []watch.Event{
+				makeTestEvent(5),
+				makeTestEvent(6),
+				makeTestEvent(7),
+				makeTestEvent(11),
+			},
+			err: nil,
+		},
+		{
+			name:      "survives 2 closed watches and reads 2+2+1 items skipping those with equal RV",
+			initialRV: "1",
+			watcherFunc: func(sinceResourceVersion string) (watch.Interface, error) {
+				return watch.NewProxyWatcher(closeAfterN(2, arrayToChannel(fromRV(sinceResourceVersion, []watch.Event{
+					makeTestEvent(1),
+					makeTestEvent(2),
+					makeTestEvent(5),
+					makeTestEvent(6),
+					makeTestEvent(7),
+					makeTestEvent(11),
+				})))), nil
+			},
+			watchCount: 3,
+			result: []watch.Event{
+				makeTestEvent(2),
+				makeTestEvent(5),
+				makeTestEvent(6),
+				makeTestEvent(7),
+				makeTestEvent(11),
+			},
+			err: nil,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			counter, watchFunc := withCounter(tc.watcherFunc)
+			watcher := NewRetryWatcher(tc.initialRV, watchFunc)
+
+			var result []watch.Event
+			event := watch.Event{Type: watch.Added}
+		loop:
+			for {
+				select {
+				case e, ok := <-watcher.ResultChan():
+					if !ok {
+						t.Errorf("Failed to read event: channel is already closed!")
+						return
+					}
+
+					if event.Type == watch.Error {
+						t.Errorf("received event %#v after error: %#v", e, event)
+						return
+					}
+
+					event = e
+
+					result = append(result, event)
+				case <-time.After(time.Second * 1):
+					// All the events are buffered -> this means we are done
+					// Also the one sec will make sure that we would detect RetryWatcher's incorrect behaviour after last event
+					watcher.Stop()
+					break loop
+				}
+
+				if event.Type == watch.Error {
+					continue
+				}
+			}
+
+			if event.Type == watch.Error {
+				if tc.err == nil {
+					t.Errorf("unexpected error: %#v", event.Object)
+					return
+				}
+
+				err, ok := event.Object.(error)
+				if !ok {
+					t.Errorf("unexpected error type: %#v", event.Object)
+					return
+				}
+
+				if err != tc.err {
+					t.Errorf("expected error '%v', got '%v'", tc.err, err)
+					return
+				}
+
+				if len(result) >= 1 {
+					result = result[:len(result)-1]
+				}
+			} else {
+				if tc.err != nil {
+					t.Errorf("expected error %v, but succeeded", tc.err)
+					return
+				}
+			}
+
+			if atomic.LoadUint32(counter) != tc.watchCount {
+				t.Errorf("expected %d watcher starts, but it has started %d times", tc.watchCount, *counter)
+			}
+
+			if !reflect.DeepEqual(result, tc.result) {
+				t.Errorf("\nexpected: %#v,\ngot:      %#v", tc.result, result)
+			}
+		})
+	}
+}
